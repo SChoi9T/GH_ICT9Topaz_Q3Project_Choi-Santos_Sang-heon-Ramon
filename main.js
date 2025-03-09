549eeb8856775f3897e934884c3057c575c92d78
Sang-heon <choi.sg@obmontessori.edu.ph>
2025-03-09 14:58:37 +0800

    index.html

--- main.js
+++ main.js
@@ -1,36 +1,61 @@
     // Regular Fare
     function regular(){
-        var l = document.getElementById("leave").value;
-        var g = document.getElementById("going").value;
+        let stationValues = {
+            "North Avenue": 4.4,
+            "Quezon Avenue": 3.5,
+            "Kamuning": 4.3,
+            "Araneta Center-Cubao": 3.9,
+            "Santolan-Annapolis": 3.2,
+            "Ortigas": 4.0,
+            "Shaw Boulevard": 4.6,
+            "Boni": 3.4,
+            "Guadalupe": 2.9,
+            "Buendia": 3.8,
+            "Ayala": 4.1,
+            "Magallanes": 2.7,
+            "Taft Avenue": 4.2,
+        };
 
-        if (l === "North Avenue") {cost = 4.4;}
-		if (l === "Quezon Avenue") {cost = 115000;}
-		if (l === "Kamuning") {cost = 17400;}
-        if (l === "Araneta Center-Cubao") {cost = 17400;}
-        if (l === "Santolan-Annapolis") {cost = 17400;}
-        if (l === "Ortigas") {cost = 17400;}
-        if (l === "Shaw Boulevard") {cost = 17400;}
-        if (l === "Boni") {cost = 17400;}
-        if (l === "Guadalupe") {cost = 17400;}
-        if (l === "Buendia") {cost = 17400;}
-        if (l === "Ayala") {cost = 17400;}
-        if (l === "Magallanes") {cost = 17400;}
-        if (l === "Taft Avenue") {cost = 17400;}
+        var fromStation = document.getElementById("from").value;
+        var toStation = document.getElementById("to").value;
 
-		if (g === "North Avenue") {cost1 = 4.4;}
-		if (g === "Quezon Avenue") {cost1 = 115000;}
-		if (g === "Kamuning") {cost1 = 17400;}
-        if (g === "Araneta Center-Cubao") {cost1 = 17400;}
-        if (g === "Santolan-Annapolis") {cost1 = 17400;}
-        if (g === "Ortigas") {cost1 = 17400;}
-        if (g === "Shaw Boulevard") {cost1 = 17400;}
-        if (g === "Boni") {cost1 = 17400;}
-        if (g === "Guadalupe") {cost1 = 17400;}
-        if (g === "Buendia") {cost1 = 17400;}
-        if (g === "Ayala") {cost1 = 17400;}
-        if (g === "Magallanes") {cost1 = 17400;}
-        if (g === "Taft Avenue") {cost1 = 17400;}
+        var baseFare = 13;
+        var perKmRate = 2;
 
-        var ans = cost/cost1;
-        window.alert('Your ticket price is ' + ans + '!');
+        let distance = Math.abs(stationValues[fromStation] - stationValues[toStation]);
+        let totalFare = baseFare + (distance * perKmRate);
+
+        document.getElementById("price").innerHTML = "TICKET PRICE: " + totalFare.toFixed(2) + " PHP";
+    }
+
+    // Discounted Fare
+    function discounted(){
+        let stationValues = {
+            "North Avenue": 4.4,
+            "Quezon Avenue": 3.5,
+            "Kamuning": 4.3,
+            "Araneta Center-Cubao": 3.9,
+            "Santolan-Annapolis": 3.2,
+            "Ortigas": 4.0,
+            "Shaw Boulevard": 4.6,
+            "Boni": 3.4,
+            "Guadalupe": 2.9,
+            "Buendia": 3.8,
+            "Ayala": 4.1,
+            "Magallanes": 2.7,
+            "Taft Avenue": 4.2,
+        };
+
+        var fromStation = document.getElementById("from").value;
+        var toStation = document.getElementById("to").value;
+
+        var baseFare = 13;
+        var perKmRate = 2;
+
+        let distance = Math.abs(stationValues[fromStation] - stationValues[toStation]);
+        let totalFare = baseFare + (distance * perKmRate);
+
+        let discountedFare = totalFare * 0.80;
+
+        document.getElementById("price").innerHTML = "TICKET PRICE (20% off): " + discountedFare.toFixed(2) + " PHP";
     }
